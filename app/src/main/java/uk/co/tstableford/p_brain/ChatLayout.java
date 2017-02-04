0d11730c198d0b5eb4af0b50dcafe8f95daa20d4
Tim Stableford <tim.stableford@gmail.com>
2017-02-04 20:05:01 +0000

    Cleanup

--- app/src/main/java/uk/co/tstableford/p_brain/ChatLayout.java
+++ app/src/main/java/uk/co/tstableford/p_brain/ChatLayout.java
@@ -47,8 +47,7 @@ public class ChatLayout extends RelativeLayout {
     public static float convertDpToPixel(float dp, Context context){
         Resources resources = context.getResources();
         DisplayMetrics metrics = resources.getDisplayMetrics();
-        float px = dp * (metrics.densityDpi / 160f);
-        return px;
+        return dp * (metrics.densityDpi / 160f);
     }
 
 }
